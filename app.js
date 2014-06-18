eacd1e62f1f912ea5d1593cd33ec83a12c6e9d36
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-06-18 16:07:51 -0600

    Added simpledb to app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var simpledb = require('mongoose-simpledb');
+
+simpledb.init();
 
 var app = express();
 
